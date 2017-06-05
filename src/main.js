474ab47ae2059f00b33d9d14fa4c58d032aaa97d
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-06-05 01:01:17 -0700

    components

--- src/main.js
+++ src/main.js
@@ -1,37 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
-class Header extends React.Component {
-  render() {
-    return <h1 style={{color: 'red'}}>{this.props.children}</h1>;
-  }
-}
-
-class Greeter extends React.Component{
-    constructor(props) {
-    super(props);
-    this.state = { name: 'React' };
-  }
-
-  render() {
-    return (
-      <div>
-        <Header>Hello {this.state.name}</Header>
-        <input type='text' ref='name' />
-        <button onClick={this.handleGreet.bind(this)}>Greet!</button>
-      </div>
-    );
-  }
-
-  handleGreet() {
-    this.setState({ name: this.refs.name.value });
-  }
-}
-class App extends React.Component{
-    render(){
-        //return <h1> Hello React </h1>;
-        return <Greeter name="Ben"/>;
-    }
-}
-
-ReactDOM.render(<App/>, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector('#anchor'));
